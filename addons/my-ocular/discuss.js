8d26207ba37565b9027345c376c04a95f1d99728
webdev03 <75148774+webdev03@users.noreply.github.com>
2021-11-19 15:14:10 +1300

    first

--- addons/my-ocular/discuss.js
+++ addons/my-ocular/discuss.js
@@ -1,13 +1,21 @@
 export default async function ({ addon, global, console, msg }) {
-  let posts = document.querySelectorAll(".blockpost");
-
+  var posts = document.querySelectorAll(".blockpost");
+  var savedData = []
+  var savedUsernames = [];
+  var idx = 0;
   posts.forEach(async (i) => {
+    idx++;
     let username = i.querySelector(".username").innerText;
-
     let left = i.querySelector(".postleft").children[0];
 
-    const { userStatus, color } = await fetchStatus(username);
-
+    const { userStatus, color, spush } = await fetchStatus(username, savedUsernames, savedData, idx);
+    /* console.log(userStatus, color, spush); */
+    if (spush === true) {
+      // so we need to add in this user
+      /* console.log(savedUsernames,savedData) */
+      savedUsernames.push(username.toLowerCase());
+      savedData.push({ status: userStatus, color: color });
+    }
     if (userStatus) {
       let br = document.createElement("br");
       addon.tab.displayNoneWhileDisabled(br);
@@ -29,12 +37,23 @@ export default async function ({ addon, global, console, msg }) {
     }
   });
 
-  async function fetchStatus(username) {
-    const response = await fetch(`https://my-ocular.jeffalo.net/api/user/${username}`);
-    const data = await response.json();
-    return {
-      userStatus: data.status,
-      color: data.color,
-    };
+  async function fetchStatus(username, usernames, savedDatas, idx) {
+    /* console.log(username, usernames, savedDatas, idx) */
+    if (usernames.includes(username.toLowerCase())) {
+      console.log(username)
+      return {
+        userStatus: savedDatas[idx].status,
+        color: savedDatas[idx].color,
+        spush: true,
+      };
+    } else {
+      const response = await fetch(`https://my-ocular.jeffalo.net/api/user/${username}`);
+      const data = await response.json();
+      return {
+        userStatus: data.status,
+        color: data.color,
+        spush: true,
+      };
+    }
   }
 }
